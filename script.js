a7bc14266ede293d123bfbbec659456b2fcd627f
OIRNOIR <oirnoir@outlook.com>
2021-11-18 20:46:27 -0800

    yeetus becheetus

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const urlParams = new URLSearchParams(location.search);
-
-if (urlParams.has('to')){
-  window.location.replace("https://soon-tm.glitch.me/?q=" + btoa(urlParams.get('to')));
-} else if (urlParams.has('q')){
-  setTimeout(() => {
-    let url = atob(urlParams.get('q'));
-    if (!url.startsWith("http")){
-      url = "https://" + url;
-    }
-    window.location.replace(url);
-  }, 10000);
-}
+document.addEventListener('load', () => {
+  const urlParams = new URLSearchParams(location.search);
+  await window.history.replaceState(null, "", "/");
+  if (urlParams.has('to')){
+    window.location.replace("https://soon-tm.glitch.me/?q=" + btoa(urlParams.get('to')));
+  } else if (urlParams.has('q')){
+    setTimeout(() => {
+      let url = atob(urlParams.get('q'));
+      if (!url.startsWith("http")){
+        url = "https://" + url;
+      }
+      window.location.replace(url);
+    }, 10000);
+  }
+});
