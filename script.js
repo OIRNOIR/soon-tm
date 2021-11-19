6688f84b675aae0e4a5c7ca68e0e2ce3979e138d
OIRNOIR <oirnoir@outlook.com>
2021-11-18 20:49:12 -0800

    fix possibly

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('load', () => {
   const urlParams = new URLSearchParams(location.search);
-  window.history.replaceState(null, "", "/");
   if (urlParams.has('to')){
     window.location.replace("https://soon-tm.glitch.me/?q=" + btoa(urlParams.get('to')));
   } else if (urlParams.has('q')){
+    window.history.replaceState(null, "", "/");
     setTimeout(() => {
       let url = atob(urlParams.get('q'));
       if (!url.startsWith("http")){
