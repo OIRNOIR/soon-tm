9ea98a74d090df84474bf749704b0ac655e5ec2d
OIRNOIR <oirnoir@outlook.com>
2021-11-18 20:55:05 -0800

    remove console logs

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 window.onload = () => {
-  console.log("1");
   const urlParams = new URLSearchParams(location.search);
-  console.log("2");
   if (urlParams.has('to')){
-    console.log("3");
     window.location.replace("/?q=" + btoa(urlParams.get('to')));
   } else if (urlParams.has('q')){
-    console.log("4");
     window.history.replaceState(null, "", "/");
-    console.log("5");
     setTimeout(() => {
-      console.log("6");
-      let url = atob(urlParams.get('q'));
-      console.log("7");
-      if (!url.startsWith("http")){
-        console.log("8");
-        url = "https://" + url;
+      let h = atob(urlParams.get('q'));
+      if (!h.startsWith("http")){
+        h = "https://" + h;
       }
-      console.log("9");
-      window.location.href = url;
+      window.location.href = h;
     }, 10000);
   }
 };
