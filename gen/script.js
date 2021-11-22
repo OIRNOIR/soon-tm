24372a60426d21a7f1de1401cf5fa1fa0ec42460
OIRNOIR <oirnoir@outlook.com>
2021-11-21 21:02:37 -0800

    pls

--- gen/script.js
+++ gen/script.js
@@ -1,3 +1,13 @@
-window.onload = () => {
-  alert("yeet");
-};
+let currentURL = "";
+
+function generate(){
+  const url = document.getElementById("link").value;
+  const currentURL = "https://soon-tm.ml/?q=" + btoa(url);
+  document.getElementById("generated").style.display = "block";
+  document.getElementById("output").innerText = currentURL;
+}
+
+function copyLink() {
+  navigator.clipboard.writeText(currentURL);
+  alert("Copied to clipboard!");
+}
